2b2146ce773a1ae01e89dfc5b33a9eb454cc4ecb
YONGMKKK <yongmk250@gmail.com>
2025-10-10 13:20:55 +0200

    Test commit - normal to trigger workflow

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -5,6 +5,7 @@ import userEvent from '@testing-library/user-event';
 import MainContent from './MainContent';
 
 // This is a test use change
+// This is a test use change 2
 
 describe('MainContent', () => {
   it('should render a button', () => {
